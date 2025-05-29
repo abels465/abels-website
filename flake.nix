f297f5a8e67b3839643f44ea188ea5f0645d5f73
Abel Svoboda <abel465@gmail.com>
2025-05-30 00:05:53 +1200

    Update

--- flake.nix
+++ flake.nix
@@ -2,6 +2,9 @@
   description = "abels website";
 
   inputs = {
+    fenix.url = "github:nix-community/fenix/3b89d5df39afc6ef3a8575fa92d8fa10ec68c95f";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
@@ -17,10 +20,33 @@
         pkgs,
         system,
         ...
-      }: {
+      }: let
+        rustPkg = with fenix.packages.${system};
+          combine [
+            targets.wasm32-unknown-unknown.latest.rust-std
+            (latest.withComponents
+              [
+                "rust-src"
+                "rustc-dev"
+                "llvm-tools-preview"
+                "cargo"
+                "clippy"
+                "rustc"
+                "rustfmt"
+                "rust-analyzer"
+              ])
+          ];
+      in {
         devShells.default = with pkgs;
           mkShell {
-            nativeBuildInputs = [nodejs];
+            nativeBuildInputs = [
+              rustPkg
+              wasm-pack
+              nodejs
+            ];
+            LD_LIBRARY_PATH = "${lib.makeLibraryPath [
+              libgcc.lib
+            ]}";
           };
       };
     };
